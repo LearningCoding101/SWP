903155295e0a14631553d7917d359bdc29630ec4
BPL08 <locdm123@gmail.com>
2024-05-29 12:08:07 +0700

    initial Commit for this project

---
+++ BadCourts/src/main/java/com/example/BadCourts/BadCourtsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BadCourts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadCourtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadCourtsApplication.class, args);
+	}
+
+}
