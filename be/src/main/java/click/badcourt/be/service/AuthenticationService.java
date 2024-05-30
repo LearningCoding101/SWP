eb3ff5b821b7aa86a624c6426aaec61a140823d8
AnhNTH2312 <anhnthse182589@fpt.edu.vn>
2024-05-30 17:40:03 +0700

    encode password

--- be/src/main/java/click/badcourt/be/service/AuthenticationService.java
+++ be/src/main/java/click/badcourt/be/service/AuthenticationService.java
@@ -2,10 +2,14 @@ package click.badcourt.be.service;
 
 import click.badcourt.be.entity.Account;
 import click.badcourt.be.enums.RoleEnum;
+import click.badcourt.be.model.AccountResponse;
 import click.badcourt.be.model.EmailDetail;
+import click.badcourt.be.model.LoginRequest;
 import click.badcourt.be.model.RegisterRequest;
 import click.badcourt.be.repository.AuthenticationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,6 +29,8 @@ public class AuthenticationService implements UserDetailsService {
 
     @Autowired
     TokenService tokenService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public Account register(RegisterRequest reg) {
         Account account = new Account();
@@ -40,6 +46,26 @@ public class AuthenticationService implements UserDetailsService {
 //        emailDetail.setMsgBody("aaa");
 
         return authenticationRepository.save(account);
+    }
+
+    public AccountResponse login(LoginRequest loginRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getPhone(),
+                loginRequest.getPassword()
+        ));
+        // => account chuẩn
+
+        Account account = authenticationRepository.findAccountByPhone(loginRequest.getPhone());
+        String token = tokenService.generateToken(account);
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setPhone(account.getPhone());
+        accountResponse.setToken(token);
+        accountResponse.setEmail(account.getEmail());
+        accountResponse.setFullName(account.getFullName());
+        accountResponse.setRole(account.getRole());
+
+        return accountResponse;
     }
 
     @Override
