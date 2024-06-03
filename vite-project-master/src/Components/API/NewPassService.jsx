8aa467cd521dc1485f35821cad8e2c4222615389
realjeremy04 <tuankietvt04@gmail.com>
2024-06-03 18:43:48 +0700

    Add reset password

--- vite-project-master/src/Components/API/NewPassService.jsx
+++ vite-project-master/src/Components/API/NewPassService.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const emailConfirm = async (email) => {
-  const url = 'http://152.42.168.144:8080/api/forgot-password'; 
+export const newPass = async (password) => {
+  const url = 'http://152.42.168.144:8080/api/reset-password'; 
   const payload = {
-    email: email,
+    password: password,
   };
 
   try {
@@ -16,6 +16,6 @@ export const emailConfirm = async (email) => {
 
     return response.data;
   } catch (error) {
-    throw new Error(`Email unavailable: ${error.response.status} - ${error.response.data}`);
+    throw new Error(`Reset failed: ${error.response.status} - ${error.response.data}`);
   }
 };
