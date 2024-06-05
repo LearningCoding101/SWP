8d75685a3e3259d23edc4542b8d63298f310a7e2
Your Name <you@example.com>
2024-06-05 08:44:47 +0700

    feedback

--- be/src/main/java/click/badcourt/be/service/FeedbackService.java
+++ be/src/main/java/click/badcourt/be/service/FeedbackService.java
@@ -1,13 +1,18 @@
 package click.badcourt.be.service;
 
+import click.badcourt.be.model.request.FeedbackRequest;
 import click.badcourt.be.repository.FeedbackRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeedbackService {
     @Autowired
     private FeedbackRespository feedbackRespository;
 
-    public List<FE>
+    public List<FeedbackRequest> getAllFeedback() {
+        return
+    }
 }
