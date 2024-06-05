90a666921b1f1cdec42ea5eab3b9f362ffb65cba
Nguyen Ngo <you@example.com>
2024-06-05 09:23:38 +0700

    club repo

--- be/src/main/java/click/badcourt/be/repository/ClubRepository.java
+++ be/src/main/java/click/badcourt/be/repository/ClubRepository.java
@@ -2,9 +2,11 @@ package click.badcourt.be.repository;
 
 import click.badcourt.be.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
     List<Club> findClubsByDeletedFalse();
 }
