69ce8eb1e4b7037bc6599bf3b870b61c7957cf69
Nguyen Ngo <you@example.com>
2024-06-05 09:00:52 +0700

    Feedback repo

--- be/src/main/java/click/badcourt/be/repository/FeedbackRespository.java
+++ be/src/main/java/click/badcourt/be/repository/FeedbackRespository.java
@@ -1,7 +1,11 @@
 package click.badcourt.be.repository;
 
+import click.badcourt.be.entity.Club;
 import click.badcourt.be.entity.FeedBack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedbackRespository extends JpaRepository<FeedBack, Long> {
+    List<Club> findFeedBacksByIsDeletedFalse(boolean isDeleted);
 }
