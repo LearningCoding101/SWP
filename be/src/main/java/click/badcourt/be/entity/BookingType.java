6ae2ed0f1050ee4bc8db0518f195fd149fcf2e12
BPL08 <locdm123@gmail.com>
2024-06-12 20:08:58 +0700

    booking type

--- be/src/main/java/click/badcourt/be/entity/BookingType.java
+++ be/src/main/java/click/badcourt/be/entity/BookingType.java
@@ -1,6 +1,7 @@
 package click.badcourt.be.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,12 +11,13 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@JsonIgnoreProperties({"booking"})
 public class BookingType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingTypeId;
     private float bookingDiscount;
-
+    private String bookingTypeInfo;
     @OneToMany(mappedBy = "bookingType")
     List<Booking> booking;
 }
